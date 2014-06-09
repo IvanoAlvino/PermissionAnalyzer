c26c6f791bb2baca91f155ee9f9bde4c961dc85e
IvanoAlvino <ivano.alvino@gmail.com>
2014-06-09 23:40:19 +0200

    Retrieve all User app + dangerous permission
    Added all user app list, with selection of those app who will utilize
    some permission marked as “Dangerous”

--- permissionanalyzer/src/main/java/com/permisisonanalyzer/permissionanalyzer/Permission_view.java
+++ permissionanalyzer/src/main/java/com/permisisonanalyzer/permissionanalyzer/Permission_view.java
@@ -1,37 +1,78 @@
 package com.permisisonanalyzer.permissionanalyzer;
 
 import android.app.Activity;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageInfo;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.content.pm.PackageManager;
 
 import java.util.ArrayList;
+import java.util.List;
 
 
 public class Permission_view extends Activity {
+
+    String[] badPermissions = new String[] {"android.permission.INTERNET","android.permission.AUTHENTICATE_ACCOUNTS",
+                                "android.permission.READ_CALL_LOG", "android.permission.READ_LOGS", "android.permission.READ_CONTACTS",
+                                "android.permission.WRITE_SECURE_SETTINGS", "android.permission.PROCESS_OUTGOING_CALLS",
+                                "android.permission.SEND_SMS", "android.permission.READ_SOCIAL_STREAM"};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_permission_view);
-        final ListView listview = (ListView) findViewById(R.id.listview);
+        // Definitions
+        PackageManager pm = getPackageManager();
+        ArrayList<String> valori = new ArrayList<String>();
+        ListView listview = (ListView) findViewById(R.id.listview);
 
-        String[] values = new String[]{"Android", "iPhone", "WindowsMobile",
-                "Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X",
-                "Linux", "OS/2", "Ubuntu", "Windows7", "Max OS X", "Linux",
-                "OS/2", "Ubuntu", "Windows7", "Max OS X", "Linux", "OS/2",
-                "Android", "iPhone", "WindowsMobile"};
-        // fill the list
-        final ArrayList<String> list = new ArrayList<String>();
-        for (int i = 0; i < values.length; ++i) {
-            list.add(values[i]);
+        // Retrieve all applications installed by user
+        int flags = PackageManager.GET_META_DATA |
+                    PackageManager.GET_SHARED_LIBRARY_FILES |
+                    PackageManager.GET_UNINSTALLED_PACKAGES;
+        List<ApplicationInfo> installed_packages = pm.getInstalledApplications(flags);
+
+        for ( ApplicationInfo appInfo : installed_packages ) {
+            // Select only the application installed by user
+            if ( (appInfo.flags & ApplicationInfo.FLAG_SYSTEM) == 0 ) {
+                // check if possess a dangerous permission
+                try {
+                    PackageInfo pkgInfo = pm.getPackageInfo(appInfo.packageName, PackageManager.GET_PERMISSIONS);
+                    String[] requested_permission = pkgInfo.requestedPermissions;
+                    if ( dangerous_permission(requested_permission) == true) {
+                        // found an application with dangerous permission
+                        valori.add( appInfo.loadLabel(pm).toString() );     // add application name in valori
+                    }
+                } catch (PackageManager.NameNotFoundException e) {
+                    e.printStackTrace();
+                }
+            }
         }
 
-        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, list);
+        // Create the display of the list throught a standard adapter
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, valori);
         listview.setAdapter(adapter);
+    }
 
+    private boolean dangerous_permission(String[] req_permissions) {
+        boolean found = false;
+
+        if ( req_permissions != null ) {
+            for (int i = 0; i < req_permissions.length && !found; i++) {
+                for (int j = 0; j < badPermissions.length && !found; j++) {
+                    if (req_permissions[i].equals(badPermissions[j])) {
+                        found = true;
+                    }
+                }
+            }
+        }
+
+        return found;
     }
 
 
