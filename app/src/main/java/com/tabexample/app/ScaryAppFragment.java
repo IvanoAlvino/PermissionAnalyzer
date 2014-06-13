583466af52636e9f1dcfed0bd6798266ce0b889a
IvanoAlvino <ivano.alvino@gmail.com>
2014-06-13 19:17:48 +0200

    Added Tab Swype view
    Added tab view based on fragments instead of activities

---
+++ app/src/main/java/com/tabexample/app/ScaryAppFragment.java
@@ -0,0 +1,90 @@
+package com.tabexample.app;
+
+
+
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ *
+ */
+public class ScaryAppFragment extends Fragment {
+
+    String[] badPermissions = new String[] {"android.permission.INTERNET","android.permission.AUTHENTICATE_ACCOUNTS",
+            "android.permission.READ_CALL_LOG", "android.permission.READ_LOGS", "android.permission.READ_CONTACTS",
+            "android.permission.WRITE_SECURE_SETTINGS", "android.permission.PROCESS_OUTGOING_CALLS",
+            "android.permission.SEND_SMS", "android.permission.READ_SOCIAL_STREAM"};
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_scary_app, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        // Definitions
+        PackageManager pm = getActivity().getPackageManager();
+        ArrayList<String> valori = new ArrayList<String>();
+        ListView listview = (ListView) getActivity().findViewById(R.id.listview);
+
+        // Retrieve all applications installed by user
+        int flags = PackageManager.GET_META_DATA |
+                PackageManager.GET_SHARED_LIBRARY_FILES |
+                PackageManager.GET_UNINSTALLED_PACKAGES;
+        List<ApplicationInfo> installed_packages = pm.getInstalledApplications(flags);
+
+        for ( ApplicationInfo appInfo : installed_packages ) {
+            // Select only the application installed by user
+            if ( (appInfo.flags & ApplicationInfo.FLAG_SYSTEM) == 0 ) {
+                // check if possess a dangerous permission
+                try {
+                    PackageInfo pkgInfo = pm.getPackageInfo(appInfo.packageName, PackageManager.GET_PERMISSIONS);
+                    String[] requested_permission = pkgInfo.requestedPermissions;
+                    if ( dangerous_permission(requested_permission)) {
+                        // found an application with dangerous permission
+                        valori.add( appInfo.loadLabel(pm).toString() );     // add application name in valori
+                    }
+                } catch (PackageManager.NameNotFoundException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        // Create the display of the list throught a standard adapter
+        ArrayAdapter adapter = new ArrayAdapter(getActivity(), android.R.layout.simple_list_item_1, valori);
+        listview.setAdapter(adapter);
+    }
+
+    private boolean dangerous_permission(String[] req_permissions) {
+        boolean found = false;
+
+        if ( req_permissions != null ) {
+            for (int i = 0; i < req_permissions.length && !found; i++) {
+                for (int j = 0; j < badPermissions.length && !found; j++) {
+                    if (req_permissions[i].equals(badPermissions[j])) {
+                        found = true;
+                    }
+                }
+            }
+        }
+
+        return found;
+    }
+}
---
+++ app/src/main/java/com/tabexample/app/TabExample.java
@@ -0,0 +1,98 @@
+package com.tabexample.app;
+
+import android.app.ActionBar;
+import android.app.FragmentTransaction;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+
+
+public class TabExample extends FragmentActivity implements ActionBar.TabListener {
+
+    ViewPager viewPager;
+    ActionBar actionBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tab_example);
+
+        // get the ViewPager handle
+        viewPager = (ViewPager) findViewById(R.id.pager);
+        viewPager.setAdapter(new MyPageAdapter(getSupportFragmentManager()));
+        viewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                actionBar.setSelectedNavigationItem(position);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+
+        // create the actionBar and sets navigation mode to TABS
+        actionBar = getActionBar();
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+
+        // create TAB 1
+        ActionBar.Tab list_tab = actionBar.newTab();
+        list_tab.setText("Scary Apps");
+        list_tab.setTabListener(this);
+
+        // create TAB 2
+        ActionBar.Tab map_tab = actionBar.newTab();
+        map_tab.setText("Safe Apps");
+        map_tab.setTabListener(this);
+
+        // add TABS to actionBar
+        actionBar.addTab(list_tab);
+        actionBar.addTab(map_tab);
+    }
+
+    @Override
+    public void onTabSelected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+        viewPager.setCurrentItem(tab.getPosition());
+    }
+
+    @Override
+    public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+    }
+
+    @Override
+    public void onTabReselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+    }
+}
+
+class MyPageAdapter extends FragmentPagerAdapter {
+
+    MyPageAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        Fragment fragment = null;
+        switch (position) {
+            case 0: fragment = new ScaryAppFragment();
+                break;
+            case 1: fragment = new SafeAppFragment();
+                break;
+        }
+        return fragment;
+    }
+
+    @Override
+    public int getCount() {
+        return 2;
+    }
+}
